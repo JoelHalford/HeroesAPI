4a0b968d1153dba7c3a3dc7ff01b4377942215f8
Joel <topsupp@hotmail.com>
2019-02-06 16:34:37 +0000

    business rule for naughty names and character restrictions

--- src/main/java/com/qa/service/AccountServiceImp.java
+++ src/main/java/com/qa/service/AccountServiceImp.java
@@ -2,15 +2,41 @@ package com.qa.service;
 
 import javax.inject.Inject;
 
+import com.qa.domain.User;
 import com.qa.persistence.repository.AccountRepository;
+import com.qa.util.JSONUtil;
 
 public class AccountServiceImp implements AccountService {
+	
+	String[] naughtyNames = new String[] {"fool", "poobrain", "biotrash", "tithead", "butt",
+			"chadmayr", "coolboyz09", "nutmayr", "nightmayr", "mayr", "moomayr", "dipstick", "pinhead", "rustytrombone", "nutstains"};
+		
+	@Inject
+	private JSONUtil util;
 	
 	@Inject
 	private AccountRepository repo;
 	
 	
 	public String addAccount(String account) {
+		
+		User anAccount = util.getObjectForJSON(account, User.class);
+		
+		for (int i = 0; i < naughtyNames.length; i++)
+		{
+			if (anAccount.getUsername().equals(naughtyNames[i]))
+			{
+				return "Username too naughty to continue, hero!";
+			}
+			else if (anAccount.getUsername().length() < 4)
+			{
+				return "Username too short, hero!";
+			}
+			else if (anAccount.getUsername().length() > 12)
+			{
+				return "Hero, your username is too long!";
+			}
+		}
 		return repo.createAccount(account);
 	}
 
